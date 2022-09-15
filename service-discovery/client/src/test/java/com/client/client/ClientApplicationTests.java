0e9bac82127b38a2b9ca6a6ed21efa474edf2130
Clément Boudereau <cboudereau@gmail.com>
2022-09-15 11:57:17 +0200

    service-discovery: fix maven offline mode + integrate unit tests in Dockerfile

--- service-discovery/client/src/test/java/com/client/client/ClientApplicationTests.java
+++ service-discovery/client/src/test/java/com/client/client/ClientApplicationTests.java
@@ -1,9 +1,7 @@
 package com.client.client;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ClientApplicationTests {
 
 	@Test
--- service-discovery/service/src/test/java/com/helloworld/service/ServiceApplicationTests.java
+++ service-discovery/service/src/test/java/com/helloworld/service/ServiceApplicationTests.java
@@ -1,9 +1,7 @@
 package com.helloworld.service;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ServiceApplicationTests {
 
 	@Test
