8e2f3f7349e15272d3809970dceb3bed85114773
Clément Boudereau <cboudereau@gmail.com>
2022-10-17 18:47:01 +0200

    spring-integration/refac: extract session management

--- spring-integration/app/src/main/java/com/service/app/consul/session/ConsulLeaderElection.java
+++ spring-integration/app/src/main/java/com/service/app/consul/session/ConsulLeaderElection.java
@@ -17,46 +17,37 @@ import com.ecwid.consul.v1.session.model.NewSession;
 
 final class ConsulLeaderElection<T> implements Handler<T> {
 
-    private static final Logger logger = LoggerFactory.getLogger(ConsulLeaderElection.class);
+    static final class ConsulSession {
+        private final NewSession newSession = new NewSession();
+        private final ConsulClient cc;
+        private final String serviceId;
 
-    private final ConsulClient cc;
-    private final String serviceId;
-    private final String leaderKey;
-    private final Supplier<T> supplier;
-    private final NewSession newSession;
-    
-    private String sessionId = null;
-    private List<String> checkIds = null;
-    
-    public ConsulLeaderElection(final ConsulClient cc, final String service, final String serviceId, final long lockDelayInSecond, final Supplier<T> handler) {
-        this.newSession = new NewSession();
-        this.newSession.setLockDelay(lockDelayInSecond);
-        this.cc = cc;
-        this.serviceId = serviceId;
-        this.supplier = handler;
-        this.leaderKey = String.format("%s/leader", service);
-    }
+        private String sessionId = null;
+        private List<String> checkIds = null;
 
-    private static Optional<String> renewSession(final ConsulClient cc, final String sessionId) {
-        try {
-            return Optional.ofNullable(cc.renewSession(sessionId, QueryParams.DEFAULT))
-                .flatMap(x -> Optional.ofNullable(x.getValue()))
-                .flatMap(x -> Optional.ofNullable(x.getId()));
-        } catch (OperationException e) {
-            if (e.getStatusCode() == 404) return Optional.empty();
-            throw e;
+        public ConsulSession(final ConsulClient cc, final String serviceId, final long lockDelayInSecond) {
+            this.cc = cc;
+            this.serviceId = serviceId;
+            this.newSession.setLockDelay(lockDelayInSecond);
         }
-    }
-    
-    @Override
-    public Optional<Optional<T>> get() {
-        logger.debug("{} : starting leader election consul based", serviceId);
-        try {
+
+        private static Optional<String> renewSession(final ConsulClient cc, final String sessionId) {
+            try {
+                return Optional.ofNullable(cc.renewSession(sessionId, QueryParams.DEFAULT))
+                    .flatMap(x -> Optional.ofNullable(x.getValue()))
+                    .flatMap(x -> Optional.ofNullable(x.getId()));
+            } catch (OperationException e) {
+                if (e.getStatusCode() == 404) return Optional.empty();
+                throw e;
+            }
+        }
+        
+        private Optional<String> tryGetSessionId() {
             if (checkIds == null) {
                 List<String> checkIdResult = cc.getAgentChecks().getValue().values().stream().filter(x -> x.getServiceId().equals(serviceId)).map(x -> x.getCheckId()).collect(Collectors.toList());
                 if (checkIdResult.isEmpty()) {
                     logger.warn("{} : empty agent service checks returned, awaiting service and health checks registration", serviceId);
-                    return Optional.of(Optional.empty());
+                    return Optional.empty();
                 }
                 checkIdResult.add("serfHealth");
                 checkIds = checkIdResult;
@@ -75,15 +66,46 @@ final class ConsulLeaderElection<T> implements Handler<T> {
                     logger.debug("{}/{}: session created", serviceId, sId);
                     return sId;
                 });
+    
+            return Optional.of(sessionId);
+        }    
+    }
 
-            PutParams acquireSessionParams = new PutParams();
-            acquireSessionParams.setAcquireSession(sessionId);
-            if (cc.setKVValue(leaderKey, sessionId, acquireSessionParams).getValue()) {
-                logger.debug("{}: leader", serviceId);
-                return Optional.of(Optional.ofNullable(this.supplier.get()));
-            } 
-            logger.debug("{}: not leader", serviceId);
-            return Optional.of(Optional.empty());
+    private static final Logger logger = LoggerFactory.getLogger(ConsulLeaderElection.class);
+
+    private final ConsulClient cc;
+    private final ConsulSession consulSession;
+    private final String serviceId;
+    private final String leaderKey;
+    private final Supplier<T> supplier;
+
+    public ConsulLeaderElection(final ConsulClient cc, final String service, final String serviceId, final long lockDelayInSecond, final Supplier<T> handler) {
+        this.cc = cc;
+        this.serviceId = serviceId;
+        this.supplier = handler;
+        this.consulSession = new ConsulSession(cc, serviceId, lockDelayInSecond);
+        this.leaderKey = String.format("%s/leader", service);
+    }
+
+    @Override
+    public Optional<Optional<T>> get() {
+        logger.debug("{} : starting leader election consul based", serviceId);
+        try {
+            Optional<String> sessionIdO = consulSession.tryGetSessionId();
+
+            if (sessionIdO.isEmpty()) {
+                return Optional.of(Optional.empty());
+            } else {
+                String sessionId = sessionIdO.get();
+                PutParams acquireSessionParams = new PutParams();
+                acquireSessionParams.setAcquireSession(sessionId);
+                if (cc.setKVValue(leaderKey, sessionId, acquireSessionParams).getValue()) {
+                    logger.debug("{}: leader", serviceId);
+                    return Optional.of(Optional.ofNullable(this.supplier.get()));
+                } 
+                logger.debug("{}: not leader", serviceId);
+                return Optional.of(Optional.empty());
+            }
         } 
         catch (ConsulException e) {
             logger.error("{}: error while managing leader", serviceId, e);
