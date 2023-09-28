2f7394964b557c9537134c3925bef7a765602809
Clément Boudereau <cboudereau@gmail.com>
2023-09-28 16:42:57 +0200

    update dependencies

--- service-discovery/env.sh
+++ service-discovery/env.sh
@@ -5,3 +5,5 @@ export JDK_IMAGE=openjdk:17-alpine
 export DISTRO=alpine:3.18.3
 export OTEL_JAVA_AGENT_VERSION=v1.30.0
 export PROMETHEUS_VERSION=v2.47.0
+export OTEL_CONTRIB_COL=0.78.0
+export VECTOR_VERSION=0.29.1-alpine
