0e4b2d1b784db2ca7409337676d00df60d6df5b4
Clément Boudereau <cboudereau@gmail.com>
2023-11-13 14:47:14 +0100

    bump new version

--- service-discovery/env.sh
+++ service-discovery/env.sh
@@ -1,9 +1,9 @@
 set -eu
 
-export CONSUL_VERSION=1.16.2
+export CONSUL_VERSION=1.17.0
 export JDK_IMAGE=openjdk:17-alpine
-export DISTRO=alpine:3.18.3
-export OTEL_JAVA_AGENT_VERSION=v1.30.0
-export PROMETHEUS_VERSION=v2.47.0
-export OTEL_CONTRIB_COL=0.78.0
-export VECTOR_VERSION=0.29.1-alpine
+export DISTRO=alpine:3.18.4
+export OTEL_JAVA_AGENT_VERSION=v1.31.0
+export PROMETHEUS_VERSION=v2.47.1
+export OTEL_CONTRIB_COL=0.88.0
+export VECTOR_VERSION=0.34.0-alpine
